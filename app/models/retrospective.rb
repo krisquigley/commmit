85e10620a236bf3253b30327f5d2941492cfa999
krisquigley <kris@krisquigley.co.uk>
2019-06-11 18:08:25 +0100

    removed validation for team happiness

--- app/models/retrospective.rb
+++ app/models/retrospective.rb
@@ -1,7 +1,9 @@
 class Retrospective < ApplicationRecord
-  validates :role_happiness, :team_happiness, :company_happiness, :feedback,
+  # TODO: Add validations for team happiness and db constraint
+
+  validates :role_happiness, :company_happiness, :feedback,
             :happiness_goal, presence: true
-  validates :role_happiness, :team_happiness, :company_happiness, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  validates :role_happiness, :company_happiness, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 
   belongs_to :sprint
   belongs_to :user
