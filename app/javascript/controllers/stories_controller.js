7795c430d3605b397d6e8b032812d7e48643a438
Kris Quigley <kris@krisquigley.co.uk>
2021-04-05 20:33:45 +0000

    Auto shows relevent checkbox when editing a repeatable story

--- app/javascript/controllers/stories_controller.js
+++ app/javascript/controllers/stories_controller.js
@@ -1,7 +1,14 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
-  static targets = ['autoAdd'];
+  static targets = ['autoAdd', 'repeatable'];
+
+  initialize() {
+    if (this.repeatableTarget.value === 'true') {
+      console.log('here');
+      this.autoAddTarget.style.display = 'block';
+    }
+  }
 
   showAutoAdd() {
     this.autoAddTarget.style.display = 'block';
