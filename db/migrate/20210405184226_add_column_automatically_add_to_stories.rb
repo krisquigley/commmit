631ef9ab4b52717b24c97106bfc2fd1ad2d1b454
Kris Quigley <kris@krisquigley.co.uk>
2021-04-05 20:21:01 +0000

    Changes story icon to reduce confusion with future significant steps icon

--- db/migrate/20210405184226_add_column_automatically_add_to_stories.rb
+++ db/migrate/20210405184226_add_column_automatically_add_to_stories.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddColumnAutomaticallyAddToStories < ActiveRecord::Migration[6.1]
   def change
     add_column :stories, :automatically_add, :boolean, default: false
