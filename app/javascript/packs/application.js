6da801aeb1c95f4ca92cd261ab8ab5c3acc6e423
Kris Quigley <kris@krisquigley.co.uk>
2021-04-12 12:50:33 +0000

    Remove image import

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,3 @@
 import '../application/stylesheets/index.js';
 import '../application/javascripts/index.js';
 import '../controllers/index.js';
-
-function importAll(r) {
-  r.keys().forEach(r);
-}
-
-// Add relevant file extensions as needed below.
-// I'm sure there is a better way :shrug:
-importAll(require.context('../images/', true, /\.(svg|jpg)$/));
