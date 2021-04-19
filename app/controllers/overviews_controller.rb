fff2751640b3733c9bf966b60a7e9490ec210e0f
Kris Quigley <kris@krisquigley.co.uk>
2021-04-19 07:36:14 +0000

    fix: remove null values from productivtiy data

--- app/controllers/overviews_controller.rb
+++ app/controllers/overviews_controller.rb
@@ -2,6 +2,6 @@
 
 class OverviewsController < ApplicationController
   def show
-    @seven_recent_commmits = Commmit.includes(:planned_stories).kept.most_recent_first.limit(7)
+    @seven_recent_commmits = Commmit.includes(:planned_stories).kept.most_recent_first.limit(8)
   end
 end
