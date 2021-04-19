9dadca0c56fc46b50dc36f4db42400b67c21f4c6
Kris Quigley <kris@krisquigley.co.uk>
2021-04-19 02:04:56 +0100

    feat: adds basic productivity analytics (#53)

---
+++ app/controllers/overviews_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class OverviewsController < ApplicationController
+  def show
+    @seven_recent_commmits = Commmit.includes(:planned_stories).kept.most_recent_first.limit(7)
+  end
+end
---
+++ app/javascript/controllers/overview_controller.js
@@ -0,0 +1,61 @@
+import { Controller } from 'stimulus';
+import ApexCharts from 'apexcharts';
+
+export default class extends Controller {
+  static targets = ['productivityData', 'chart'];
+
+  connect() {
+    const options = {
+      theme: {
+        mode: 'dark',
+        palette: 'palette1',
+      },
+      chart: {
+        background: 'none',
+        toolbar: {
+          show: false,
+        },
+        type: 'line',
+      },
+      stroke: {
+        curve: 'smooth',
+      },
+      series: [
+        {
+          name: 'Productivity',
+          data: JSON.parse(this.productivityDataTarget.value),
+        },
+      ],
+      grid: {
+        show: false,
+      },
+      yaxis: {
+        logarithmic: true,
+        axisTicks: {
+          show: false,
+        },
+        labels: {
+          show: false,
+        },
+      },
+      xaxis: {
+        tooltip: {
+          enabled: false,
+        },
+        labels: {
+          show: false,
+        },
+        axisBorder: {
+          show: false,
+        },
+        axisTicks: {
+          show: false,
+        },
+      },
+    };
+
+    const chart = new ApexCharts(this.chartTarget, options);
+
+    chart.render();
+  }
+}
