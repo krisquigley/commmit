a95bb148e2cf37d4829165225e157a7508dbb31e
Kris Quigley <kris@krisquigley.co.uk>
2021-05-25 21:39:42 +0000

    feat: shows productivity on reflections

--- app/helpers/commmits_helper.rb
+++ app/helpers/commmits_helper.rb
@@ -9,4 +9,9 @@ module CommmitsHelper
   def completed_planned_stories(commmit)
     commmit.planned_stories.count(&:completed_at)
   end
+
+  def show_productivity?
+    controller.controller_name == 'planned_stories' && controller.action_name == 'index' ||
+      controller.controller_name == 'reflections'
+  end
 end
