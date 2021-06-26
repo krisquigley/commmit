2bd5012856fcaa5f374652c83361c3419896513d
Kris Quigley <kris@krisquigley.co.uk>
2021-06-26 14:31:51 +0000

    fix: dates for commmits ending today

--- app/javascript/controllers/relative-time_controller.js
+++ app/javascript/controllers/relative-time_controller.js
@@ -3,25 +3,29 @@ import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
 import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.extend(relativeTime);
+dayjs.extend(isSameOrAfter);
 dayjs.extend(isSameOrBefore);
-
 export default class extends Controller {
   static targets = ['relativeTime', 'commmit'];
 
   connect() {
     this.relativeTimeTargets.forEach((target) => {
       const commmit = this.hasCommmitTarget ? 'day' : false;
-      const today = dayjs().isSame(dayjs(target.title), commmit);
-      const past = dayjs().isAfter(dayjs(target.title), commmit);
+      const todayOrPast = dayjs().isSameOrAfter(dayjs(target.title), commmit);
+      const sameOrBefore = dayjs().isSameOrBefore(dayjs(target.title));
 
-      if (today) {
+      if (commmit && todayOrPast) {
+        target.innerHTML = dayjs(target.title)
+          .tz('Europe/London')
+          .endOf('day')
+          .fromNow();
+      } else if (sameOrBefore && !commmit) {
         target.innerHTML = dayjs(target.title).tz('Europe/London').toNow();
-      } else if (past) {
-        target.innerHTML = dayjs(target.title).tz('Europe/London').fromNow();
       } else {
         target.innerHTML = dayjs(target.title)
           .add(1, commmit)
