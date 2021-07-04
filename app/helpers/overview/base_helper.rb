f704254e6099c9447799c1b78f1cf4e5217f7da3
Kris Quigley <kris@krisquigley.co.uk>
2021-07-04 15:27:26 +0000

    lint: layout changes

--- app/helpers/overview/base_helper.rb
+++ app/helpers/overview/base_helper.rb
@@ -1,19 +1,23 @@
-module Overview::BaseHelper
-  def calculate_average(values)
-    values.size.zero? ? nil : (values.sum / (values.size * 1.0)).ceil(2)
-  end
-  
-  def date_range
-    Range.new(Time.current.to_date - 7.days, Time.current.to_date - 1.day)
-  end
+# frozen_string_literal: true
 
-  def formatted_date_range
-    Oj.dump(date_range.map { |date| date.strftime("%a, %B #{date.day.ordinalize}") })
-  end
+module Overview
+  module BaseHelper
+    def calculate_average(values)
+      values.size.zero? ? nil : (values.sum / (values.size * 1.0)).ceil(2)
+    end
 
-  private
+    def date_range
+      Range.new(Time.current.to_date - 7.days, Time.current.to_date - 1.day)
+    end
 
-  def find_commmit_for_date(date)
-    @seven_recent_commmits.find { |c| c.end_date == date }
+    def formatted_date_range
+      Oj.dump(date_range.map { |date| date.strftime("%a, %B #{date.day.ordinalize}") })
+    end
+
+    private
+
+    def find_commmit_for_date(date)
+      @seven_recent_commmits.find { |c| c.end_date == date }
+    end
   end
 end
--- app/helpers/overview/goals_helper.rb
+++ app/helpers/overview/goals_helper.rb
@@ -1,12 +1,16 @@
-module Overview::GoalsHelper
-  def average_goals_met
-    reflections = @seven_recent_commmits.map(&:reflection)
-    total_reflections = reflections.size
+# frozen_string_literal: true
 
-    return nil if total_reflections.size.zero?
+module Overview
+  module GoalsHelper
+    def average_goals_met
+      reflections = @seven_recent_commmits.map(&:reflection)
+      total_reflections = reflections.size
 
-    goals_met = reflections.count { |reflection| reflection&.goal_met == true }
+      return nil if total_reflections.size.zero?
 
-    ((goals_met / (total_reflections * 1.0)) * 100).ceil(0)
+      goals_met = reflections.count { |reflection| reflection&.goal_met == true }
+
+      ((goals_met / (total_reflections * 1.0)) * 100).ceil(0)
+    end
   end
 end
