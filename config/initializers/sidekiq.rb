7732d129d73815dbc2c26051fd76ef3be89bfe8d
Kris Quigley <kris@krisquigley.co.uk>
2021-08-26 21:40:06 +0000

    Revert "feat: use SSL for redis in prod"
    This reverts commit 2added202077f6c5947ef384e679609876ff8746.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,17 +1,9 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = if Rails.env.production?
-                   { url: ENV.fetch('REDIS_TLS_URL') }
-                 else
-                   { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
-                 end
+  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = if Rails.env.production?
-                   { url: ENV.fetch('REDIS_TLS_URL') }
-                 else
-                   { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
-                 end
+  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
 end
