2added202077f6c5947ef384e679609876ff8746
Kris Quigley <kris@krisquigley.co.uk>
2021-08-26 21:30:04 +0000

    feat: use SSL for redis in prod

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+  config.redis = if Rails.env.production?
+                   { url: ENV.fetch('REDIS_TLS_URL') }
+                 else
+                   { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+                 end
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+  config.redis = if Rails.env.production?
+                   { url: ENV.fetch('REDIS_TLS_URL') }
+                 else
+                   { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+                 end
 end
